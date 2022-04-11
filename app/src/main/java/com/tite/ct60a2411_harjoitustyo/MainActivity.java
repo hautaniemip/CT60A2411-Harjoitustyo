3f4dd13e4b42d0d6f5aaf234316922dc1e77f7c1
Pekka Hautaniemi <hautaniemip@gmail.com>
2022-04-12 00:29:41 +0300

    Async XML reading
    Todo: Add comments to class and clean it up

--- app/src/main/java/com/tite/ct60a2411_harjoitustyo/MainActivity.java
+++ app/src/main/java/com/tite/ct60a2411_harjoitustyo/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private String url = "https://www.finnkino.fi/xml/Schedule/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] tags = {"ID"};
+        new XMLReaderTask(this, url, "Show", tags).execute();
+    }
+
+    public void dataCallback(ArrayList<String[]> result) {
+        System.out.println("###### TEST #####");
+        System.out.println(result.get(0)[0]);
     }
 }
