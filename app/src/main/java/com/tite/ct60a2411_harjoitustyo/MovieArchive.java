2ee678310c16aeeadaeca1ea77546138d83275d2
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-04-22 16:10:27 +0300

    Added MovieArchive Class

---
+++ app/src/main/java/com/tite/ct60a2411_harjoitustyo/MovieArchive.java
@@ -0,0 +1,46 @@
+package com.tite.ct60a2411_harjoitustyo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class MovieArchive implements Serializable {
+    private static MovieArchive instance;
+    private ArrayList<Movie> movies = new ArrayList<>();
+
+    public MovieArchive() {
+
+    }
+
+    public static MovieArchive getInstance() {
+        if (instance == null) {
+            instance = (MovieArchive) ObjectSaveUtils.readObject("MovieArchive.data");
+            if (instance == null)
+                instance = new MovieArchive();
+        }
+
+        return instance;
+    }
+
+    public void saveArchive() {
+        ObjectSaveUtils.saveObject(instance, "MovieArchive.data");
+    }
+
+    public void addMovie(Movie movie) {
+        if (movies.isEmpty()) {
+            movies.add(movie);
+            return;
+        }
+
+        for (Movie movieIt : movies)
+            if (movieIt.getEventId() == movie.getEventId())
+                return;
+
+        movies.add(movie);
+    }
+
+    public void printArchiveInfo() {
+        System.out.println("MovieArchive size: " + movies.size());
+    }
+
+
+}
