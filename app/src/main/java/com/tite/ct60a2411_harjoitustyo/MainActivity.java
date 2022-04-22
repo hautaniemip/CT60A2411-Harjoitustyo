1e8d1b4140d695eabfd2d0d21b4522a7a2ec085b
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-04-22 13:43:01 +0300

    Changed XML reading in app start to sequential

--- app/src/main/java/com/tite/ct60a2411_harjoitustyo/MainActivity.java
+++ app/src/main/java/com/tite/ct60a2411_harjoitustyo/MainActivity.java
@@ -8,23 +8,36 @@ import java.security.cert.CertPathValidatorException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    protected static TheatreArea.AreaId areaId = TheatreArea.AreaId.STRAND;
-    protected static ArrayList<TheatreArea> areas = new ArrayList<>();
-    private String url = "https://www.finnkino.fi/xml/Schedule/";
+    private static TheatreArea.AreaId areaId = TheatreArea.AreaId.STRAND;
+    private static ArrayList<TheatreArea> areas = new ArrayList<>();
+    private static String[] tags = {"ID", "dttmShowStart", "dttmShowEnd", "Title", "OriginalTitle", "ProductionYear", "LengthInMinutes", "Rating", "TheatreID", "Theatre", "TheatreAuditorium"};
+    private static String url;
+    private static int areaIndex = 0;
+    private static MainActivity context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
 
-        String[] tags = {"ID", "dttmShowStart", "dttmShowEnd", "Title", "OriginalTitle", "ProductionYear", "LengthInMinutes", "Rating", "TheatreID", "Theatre", "TheatreAuditorium"};
+        readAllAreas();
+    }
 
-        for (TheatreArea.AreaId areaId : TheatreArea.AreaId.values()) {
-            url = "https://www.finnkino.fi/xml/Schedule/?area=" + areaId.getId();
-            XMLReaderTask reader = new XMLReaderTask(this, url, "Show", tags);
-            reader.setCallback(MainActivity::dataCallback);
-            reader.execute();
-        }
+    // Read XML for every area sequentially
+    public static void readAllAreas() {
+        if (areaIndex == TheatreArea.AreaId.values().length)
+            return;
+
+        TheatreArea.AreaId areaId = TheatreArea.AreaId.values()[areaIndex];
+
+        url = "https://www.finnkino.fi/xml/Schedule/?area=" + areaId.getId();
+        XMLReaderTask reader = new XMLReaderTask(context, url, "Show", tags);
+        reader.setCallback(MainActivity::dataCallback);
+        reader.execute();
+
+        areaIndex++;
     }
 
     public static void dataCallback(ArrayList<String[]> result) {
@@ -36,5 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         areas.add(area);
         area.printAreaInfo();
+
+        readAllAreas();
     }
 }
