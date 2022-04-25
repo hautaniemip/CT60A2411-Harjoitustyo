7e34fea28cdaed9b5a1f286b3ec2cda74c0ab453
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-04-26 00:40:20 +0300

    Made MovieArrayAdapter filterable

--- app/src/main/java/com/tite/ct60a2411_harjoitustyo/MovieArrayAdapter.java
+++ app/src/main/java/com/tite/ct60a2411_harjoitustyo/MovieArrayAdapter.java
@@ -8,59 +8,94 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MovieArrayAdapter extends BaseAdapter implements ListAdapter {
+public class MovieArrayAdapter extends BaseAdapter implements Filterable {
     private final ArrayList<Movie> list;
     private final Context context;
+    private final LayoutInflater inflater;
+    private ArrayList<Movie> filteredList;
 
     public MovieArrayAdapter(Context context, ArrayList<Movie> list) {
         this.list = list;
+        this.filteredList = list;
         this.context = context;
+        inflater = LayoutInflater.from(context);
     }
 
     @Override
     public int getCount() {
-        return list.size();
+        return filteredList.size();
     }
 
     @Override
     public Object getItem(int pos) {
-        return list.get(pos);
+        return filteredList.get(pos);
     }
 
     @Override
     public long getItemId(int pos) {
-        return list.get(pos).getID();
+        return filteredList.get(pos).getID();
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        if (view == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (view == null)
             view = inflater.inflate(R.layout.layout_movie, null);
-        }
 
         TextView movieTitle = view.findViewById(R.id.movieTitle);
         TextView movieText = view.findViewById(R.id.movieText);
         Button showButton = view.findViewById(R.id.showButton);
 
-        movieTitle.setText(list.get(i).getTitle());
-        movieText.setText(list.get(i).toString());
+        movieTitle.setText(filteredList.get(i).getTitle());
+        movieText.setText(filteredList.get(i).toString());
 
         showButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.getContext(), MovieActivity.class);
                 Bundle bundle = new Bundle();
-                bundle.putSerializable("movie", list.get(i));
+                bundle.putSerializable("movie", filteredList.get(i));
                 intent.putExtras(bundle);
                 context.startActivity(intent);
             }
         });
         return view;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String filterString = constraint.toString().toLowerCase();
+                FilterResults results = new FilterResults();
+
+                final ArrayList<Movie> filteringList = list;
+                int count = filteringList.size();
+                final ArrayList<Movie> filteredList1 = new ArrayList<>(count);
+
+                for (Movie movie : filteringList) {
+                    String movieTitle = movie.getTitle();
+                    if (movieTitle.toLowerCase().contains(filterString))
+                        filteredList1.add(movie);
+                }
+
+                results.values = filteredList1;
+                results.count = filteredList1.size();
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredList = (ArrayList<Movie>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 }
