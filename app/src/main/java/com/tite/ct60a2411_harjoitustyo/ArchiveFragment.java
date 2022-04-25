cf970f3ea6ec2ae6d538ac6fd8dd1bfca453a792
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-04-25 15:49:15 +0300

    Added fragment rendering

--- app/src/main/java/com/tite/ct60a2411_harjoitustyo/ArchiveFragment.java
+++ app/src/main/java/com/tite/ct60a2411_harjoitustyo/ArchiveFragment.java
@@ -1,4 +1,17 @@
 package com.tite.ct60a2411_harjoitustyo;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class ArchiveFragment extends androidx.fragment.app.Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_archive, container, false);
+    }
 }
