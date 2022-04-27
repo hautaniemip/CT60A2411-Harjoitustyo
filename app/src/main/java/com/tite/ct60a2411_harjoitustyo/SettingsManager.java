f312c588a3e97437e3564fd9e9e7c367424e2427
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-04-27 22:36:30 +0300

    Added settings saving to SettigsManager

--- app/src/main/java/com/tite/ct60a2411_harjoitustyo/SettingsManager.java
+++ app/src/main/java/com/tite/ct60a2411_harjoitustyo/SettingsManager.java
@@ -8,17 +8,23 @@ public class SettingsManager implements Serializable {
     private int fontSize;
     private int updateArchiveLength;
 
-    public SettingsManager() {
+    private SettingsManager() {
         this.languageIndex = 0;
         this.fontSize = 16;
         this.updateArchiveLength = 14;
     }
 
-    public SettingsManager getInstance() {
+    public static SettingsManager getInstance() {
         if (instance == null) {
-            instance = new SettingsManager();
+            instance = (SettingsManager) ObjectSaveUtils.readObject("SettingsManager.data");
+            if (instance == null)
+                instance = new SettingsManager();
         }
         return instance;
+    }
+
+    public void saveArchive() {
+        ObjectSaveUtils.saveObject(instance, "SettingsManager.data");
     }
 
     public int getLanguageIndex() {
