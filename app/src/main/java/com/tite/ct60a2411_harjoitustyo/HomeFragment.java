d7cbe4d84f2a1da61841c6e88e996cf5b5222c7a
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2022-04-29 23:06:22 +0300

    Added buttons to first two movies of the day to the front page

--- app/src/main/java/com/tite/ct60a2411_harjoitustyo/HomeFragment.java
+++ app/src/main/java/com/tite/ct60a2411_harjoitustyo/HomeFragment.java
@@ -1,10 +1,12 @@
 package com.tite.ct60a2411_harjoitustyo;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -14,13 +16,19 @@ import androidx.fragment.app.Fragment;
 
 import java.io.InputStream;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+    private Button movie1Button;
+    private Button movie2Button;
+    private ArrayList<Movie> movies;
+
+    private SettingsManager settingsManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+        settingsManager = SettingsManager.getInstance();
         return inflater.inflate(R.layout.fragment_main, container, false);
 
     }
@@ -39,6 +47,17 @@ public class HomeFragment extends Fragment {
             }
         });
         thread.start();
+
+        movie1Button = (Button) view.findViewById(R.id.movie1Button);
+        movie2Button = (Button) view.findViewById(R.id.movie2Button);
+
+        String url = "https://www.finnkino.fi/xml/Schedule/?area=" + settingsManager.getHomeArea();
+
+        XMLReaderTask reader = new XMLReaderTask(MainActivity.getContext(), url, "Show", MainActivity.getTags());
+        reader.setShowDialog(false);
+        reader.setCallback(this::dataCallback);
+        reader.execute();
+
     }
 
     public static Drawable LoadImageFromWebOperations(String url) {
@@ -53,4 +72,37 @@ public class HomeFragment extends Fragment {
         }
     }
 
+    public void dataCallback(ArrayList<String[]> result) {
+        if (result == null)
+            return;
+
+        movies = new ArrayList<>();
+
+        for (String[] entry : result)
+            movies.add(new Movie(entry));
+
+        movie1Button.setText(movies.get(0).getTitle());
+        movie1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.getContext(), MovieActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("movie", movies.get(0));
+                intent.putExtras(bundle);
+                MainActivity.getContext().startActivity(intent);
+            }
+        });
+
+        movie2Button.setText(movies.get(1).getTitle());
+        movie2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.getContext(), MovieActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("movie", movies.get(1));
+                intent.putExtras(bundle);
+                MainActivity.getContext().startActivity(intent);
+            }
+        });
+    }
 }
